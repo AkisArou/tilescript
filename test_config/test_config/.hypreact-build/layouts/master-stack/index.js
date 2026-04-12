a6098f96252c129b3692654b45a3b389605fb25e
AkisArou <akisarou90@gmail.com>
2026-04-12 19:14:06 +0300

    test_config

--- test_config/test_config/.hypreact-build/layouts/master-stack/index.js
+++ test_config/test_config/.hypreact-build/layouts/master-stack/index.js
@@ -1,17 +1,11 @@
 import { sp } from "@hypreact/sdk/jsx-runtime";
 
 export default function layout(ctx) {
-	return /* @__PURE__ */ sp("workspace", {
-		id: "frame",
-		class: "playground-workspace"
-	}, /* @__PURE__ */ sp("slot", {
+	return /* @__PURE__ */ sp("workspace", { id: "frame" }, /* @__PURE__ */ sp("slot", {
 		id: "master",
 		take: 1,
 		class: "master-slot"
-	}), ctx.windows.length > 1 ? /* @__PURE__ */ sp("group", {
-		id: "stack",
-		class: "stack-group"
-	}, /* @__PURE__ */ sp("slot", {
+	}), ctx.windows.length > 1 ? /* @__PURE__ */ sp("group", { class: "stack-group" }, /* @__PURE__ */ sp("slot", {
 		id: "stack-slot",
 		class: "stack-group__item"
 	})) : null);
